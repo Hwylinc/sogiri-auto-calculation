e4d4606a2608279f348a4d1e043aaf2427ad96e6
motokiTim <mtk8vv1991@gmail.com>
2023-04-14 17:03:35 +0900

    計算ロジック端材と予備材追加

--- app/app/Http/Controllers/CsvCalculatorController.php
+++ app/app/Http/Controllers/CsvCalculatorController.php
@@ -105,7 +105,7 @@ class CsvCalculatorController extends Controller
             $port = $ports[rand(0, 4)];
             $size =  $sizes[rand(0, 4)];
             $length = rand(50, 400)*10;
-            $data[$size][$port.'-'.$length] = rand(10, 40);
+            $data[$size][$port.'-'.$length] = rand(10, 100);
         }
         // *****************************************************
         // テストデータ生成　End
@@ -116,7 +116,6 @@ class CsvCalculatorController extends Controller
             // 一番少ない本数を取得
             $min = $this->getMinAmount($size, $array);
             $min_renew_flg = false;
-            $lengths[] = 'test';
             // 全て切断しているかの確認ループ
             while ($min > 0) {                
                 // trueの時新たな最小値を取得する必要がある
@@ -156,7 +155,7 @@ class CsvCalculatorController extends Controller
                     }                 
                 }
                 // 切断指示作成
-                $test = $this->getCombination($lengths);
+                $test = $this->getCombination($lengths, $size);
                 echo '<br>＝＝＝＝＝＝＝同時切断：　'.$min.'本 終了 ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝ <br><br><br>';
             }
             $message = '【切断計算後の素材のリスト】';
@@ -189,7 +188,7 @@ class CsvCalculatorController extends Controller
     /*
      * 最適な組み合わせを
     */
-    public function getCombination($lengths)
+    public function getCombination($lengths, $size)
     {
         $num_lengths = count($lengths);
 
@@ -198,6 +197,9 @@ class CsvCalculatorController extends Controller
 
         $rods = [8000];
         $cuts = [];
+
+        // 予備材リスト
+        $spare_cut_list =$this->getSpareBySize($size);
 
         while (count($cut_list) > 0) {
             $best_cut_index = -1;
@@ -237,7 +239,10 @@ class CsvCalculatorController extends Controller
                 if($i != 0) {
                     echo "合計：　".$result[$cut[1]]."mm<br>";
                     $left = 8000 - $result[$cut[1]];
-                    echo "端材：　".$left."mm<br><br>";
+                    echo "仮端材：　".$left."mm<br><br>";
+                    
+                    //　予備材を使って端材を最小化
+                    $this->cutLeftWithSpare($left, $spare_cut_list);
                 }
                 echo "設置" . ($cut[1]+1) . "回目:<br>" . $cut[0] . "mm (" . ($i+1) . "カット目)<br>";
                 $count = $cut[1]+1;
@@ -252,7 +257,9 @@ class CsvCalculatorController extends Controller
         }
         echo "合計：　".end($result)."mm<br>";
         $left = 8000 - end($result);
-        echo "端材：　".$left."mm<br>";
+        echo "端材：　".$left."mm<br><br>";
+        //　予備材を使って端材を最小化
+        $this->cutLeftWithSpare($left, $spare_cut_list);
     }
 
 
@@ -269,6 +276,7 @@ class CsvCalculatorController extends Controller
         $max_limit['D22'] = 10;
     
         $min = 0;
+        $max = 0;
         // 初期の入力値で最小の本数を取得
         foreach ($array as $port_length => $amount) {
             if ($amount == 0) {
@@ -288,4 +296,68 @@ class CsvCalculatorController extends Controller
         return $min;
     }
 
+    /*
+     * 鉄筋径別の予備材リストを取得
+    */
+    public function getSpareBySize($size)
+    {
+        if($size == 'D10') {
+            $list = array(180, 280, 320, 370, 410, 550, 580, 700, 765, 800, 840, 860, 870, 900, 910, 915, 1000, 1100, 1200, 1250, 1300, 1310, 1325, 1360, 1500, 1760, 1780, 1800, 1960, 2235, 2660, 2690, 2730, 3560, 3600);
+        } elseif ($size == 'D13') {
+            $list = array(180, 250, 280, 330, 550, 860, 870, 910, 940, 1000, 1050, 1100, 1200, 1300, 1310, 1325, 1400, 1450, 1500, 1530, 1550, 1600, 1650, 1720, 1760, 1780, 1800, 1960, 2000, 2100, 2235, 2660, 2690, 3600);
+        } elseif ($size == 'D16') {
+            $list = array(420, 825, 860, 870, 1310, 1376, 1380, 1400, 1500, 1760, 1780, 1800, 1960, 2000, 2235, 2300, 2660, 2690, 3560, 3600);
+        } elseif ($size == 'D19') {
+            $list = array(930, 1580, 1680, 1710, 1800, 1830, 2200, 2230, 2280, 2580, 2730, 3630);
+        } elseif ($size == 'D22') {
+            $list = array(2000, 2400);
+        }
+        return $list;
+    }
+
+    /*
+     * 予備材リストを用いた端材の切断結果をテスト出力
+    */
+    public function cutLeftWithSpare($left, $spare_cut_list)
+    {
+        $used_spare_result = $this->findCutList($left, $spare_cut_list);
+        if ($used_spare_result === false) {
+            echo "予備材で切断できません。<br><br>";
+        } else {
+            echo "【予備材使用の計算結果】<br>";
+            $cutCount = count($spare_cut_list);
+            $waste = $left;
+            for ($j = 0; $j < $cutCount; $j++) {
+                if ($used_spare_result[$j] > 0) {
+                    echo "{$spare_cut_list[$j]}mm x {$used_spare_result[$j]}<br>";
+                    $waste -= $spare_cut_list[$j] * $used_spare_result[$j];
+                }
+            }
+            echo '<span style="color:#0000FF;">最終端材：'.$waste.'mm</span><br><br>';
+        }
+    }
+
+    /*
+     * 予備材リストを用いた端材の切断結果を取得
+    */
+    public function findCutList($left_length, $spare_cut_list) {
+        // $spare_cut_listを昇順にソートする
+        sort($spare_cut_list); 
+        // $spare_cut_listの数を取得する
+        $cut_count = count($spare_cut_list);
+        // $spare_cut_listの要素数分、0で初期化された配列を作成する
+        $cut_result = array_fill(0, $cut_count, 0); 
+        while ($left_length > 0) { // left_lengthが0になるまで繰り返す
+            $i = $cut_count - 1;
+            while ($i >= 0 && $spare_cut_list[$i] > $left_length) { // left_lengthよりも大きい$spare_cut_listの要素を探す
+                $i--;
+            }
+            if ($i < 0) { // left_lengthよりも小さい$spare_cut_listの要素がない場合、処理を中断する
+                break;
+            }
+            $cut_result[$i]++; // 切断する$spare_cut_listの要素のカウントを1増やす
+            $left_length -= $spare_cut_list[$i]; // 切断した分、left_lengthを減らす
+        }
+        return $cut_result; // 切断結果を返す
+    }
 }

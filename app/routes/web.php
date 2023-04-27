b1937f222099e3c646d233a00f17ba7b62a6067a
motokiTim <mtk8vv1991@gmail.com>
2023-04-27 16:12:56 +0900

    ログイン機能実装

--- app/routes/web.php
+++ app/routes/web.php
@@ -3,19 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-/**
- * Undocumented function
- *
- * @param [type] $request require　リクエスト内容
- * @param [type] $controller require　　routeのcontroller
- * @param [type] $exe       require 実行メソッド
- * @param [type] $value　　　パラメータ
- * @return void
- */
-function routeStore($request, $controller, $exe, $value=null) {
-    $instance = new $controller();
-    return $instance->store($request, $exe, $value);
-}
 
 /*
 |--------------------------------------------------------------------------
@@ -33,9 +20,26 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/cal-test', [App\Http\Controllers\CsvCalculatorController::class, 'calTest']);
-Route::get('/cal-second-test', [App\Http\Controllers\CsvCalculatorController::class, 'calSecondTest']);
+
+Route::middleware('auth')->group(function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/cal-test', [App\Http\Controllers\CsvCalculatorController::class, 'calTest']);
+    Route::get('/cal-second-test', [App\Http\Controllers\CsvCalculatorController::class, 'calSecondTest']);
+});
+
+/**
+ * Undocumented function
+ *
+ * @param [type] $request require　リクエスト内容
+ * @param [type] $controller require　　routeのcontroller
+ * @param [type] $exe       require 実行メソッド
+ * @param [type] $value　　　パラメータ
+ * @return void
+ */
+function routeStore($request, $controller, $exe, $value=null) {
+    $instance = new $controller();
+    return $instance->store($request, $exe, $value);
+}
 
 // 予備材一覧
 Route::get('/spare/{screen}/{select_id}', function(Request $request, $screen, $select_id) {

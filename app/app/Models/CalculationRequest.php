076552dc4064c6d89eaf2e44f2f3a941ab25f24c
motokiTim <mtk8vv1991@gmail.com>
2023-05-09 09:44:29 +0900

    鉄筋情報Seeder作成

---
+++ app/app/Models/CalculationRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CalculationRequest extends Model
+{
+    use HasFactory;
+}

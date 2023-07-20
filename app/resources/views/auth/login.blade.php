bc7712d1010d95dd4288d73adbf731c80479baee
Shota77 <urano.shota1994@gmail.com>
2023-07-20 15:27:57 +0900

    No22修正完了

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        <div class="text-center card-img">
+                            <img src="{{ asset("images/login_center_logo.svg") }}" alt="" class="head-logo">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="col-form-label text-md-end">{{ __('MAIL') }}</label>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
+                            <div>
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -25,10 +25,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="mb-5">
+                            <label for="password" class="col-form-label text-md-end">{{ __('PASS') }}</label>
 
-                            <div class="col-md-6">
+                            <div>
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -40,9 +40,9 @@
                         </div>
                         
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                            <div>
+                                <button type="submit" class="btn">
+                                    {{ __('ログイン') }}
                                 </button>
                             </div>
                         </div>
@@ -51,5 +51,26 @@
             </div>
         </div>
     </div>
+    <div class="text-center card-img">
+        <img src="{{ asset("images/login_footer_logo.svg") }}" alt="" class="head-logo">
+    </div>
 </div>
 @endsection
+
+<style>
+    .card {
+        height: 440px;
+        padding: 24px 60px;
+        
+    }
+    .btn {
+        width: 100%;
+        background: linear-gradient(90deg, #30CFC7 9.11%, #3A7EBA 89.29%) !important;
+        border-radius: 62px !important;
+        color: #ffffff !important;
+        height: 48px;
+    }
+    .card-img {
+        margin-top: -20px;
+    }
+</style>
